b82bb4b17d3c638eeca32379fedf1a4dfe00037e
ondrs <plsek.o@gmail.com>
2014-02-23 23:38:51 +0200

    test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,22 @@ if (!class_exists('Tester\Assert')) {
 	exit(1);
 }
 
+date_default_timezone_set('Europe/Prague');
+
 Tester\Environment::setup();
+
+if(extension_loaded('xdebug')) {
+    Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
+}
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
+Tester\Helpers::purge(TEMP_DIR);
 
 function id($val) {
     return $val;
+}
+
+function run(Tester\TestCase $testCase) {
+    $testCase->run(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL);
 }
